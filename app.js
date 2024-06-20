34ed40513223b19175b01305124f9bcbc8059f55
Kayky Ribeiro <kaykykaro04@gmail.com>
2024-06-19 21:52:29 -0300

    final-cursos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message, type) => {
   const wrapper = document.createElement('div')
   wrapper.innerHTML = [
-    `<div class="alert alert-${type} d-flex align-items-center" role="alert">`,
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
     `   <div>${message}</div>`,
     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
     '</div>'
@@ -19,8 +19,15 @@ if (alertTrigger) {
 }
 
 const alertTriggerNot = document.getElementById('notAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
+if (alertTriggerNot) {
+  alertTriggerNot.addEventListener('click', () => {
+    appendAlert('Você não tem dinheiro para realizar esta compra', 'danger')
+  })
+}
+
+const alertTriggerCasal = document.getElementById('liveAlertBtnCasal')
+if (alertTriggerCasal) {
+  alertTriggerCasal.addEventListener('click', () => {
     appendAlert('Parabéns, compra realizada com sucesso. Acesse seus cursos/plalinhas na suas compras na area de navegação', 'success')
   })
 }
